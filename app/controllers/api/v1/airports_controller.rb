3abbb1e86b1cb651192abf58758b48f0615f423d
MostafaHosny <mostafa.hosny2015@hotmail.com>
2024-05-06 05:05:24 +0200

    order by  biggest airport first

--- app/controllers/api/v1/airports_controller.rb
+++ app/controllers/api/v1/airports_controller.rb
@@ -1,6 +1,6 @@
 class API::V1::AirportsController < API::V1::BaseController
   def index
-    airports = Airport.all
+    airports = Airport.order('passenger_volume DESC NULLS LAST')
     airports = airports.by_country_codes(params[:countries]) if params[:countries].present?
     render json: AirportSerializer.render(airports)
   end
